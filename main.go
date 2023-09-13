87e87f8f1e6b78a81a08f76a2e552ad96f9cd5f9
yasser nadir <yassernadir761@gmail.com>
2023-09-13 10:40:06 +0100

    add comment for the delete/list problem

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 
@@ -114,27 +114,35 @@ func DeletePod(clientset *kubernetes.Clientset, podname string, namespace string
 		panic(err.Error())
 	}
 
-	fmt.Printf("Deleted Pod: %s\n", podname)
+	fmt.Printf("Deleted Pod: %s\n\n", podname)
 }
 
 
 func main(){
 	namespace := "default"
-	podName := "nginx-1"
-	imageName := "nginx"
+	podname := "nginx-1"
+	imagename := "nginx"
 	newImageName := "nginx:1.21"
 
 	config := CreateK8sClientConfig()
 	clientset := CreateK8sClientset(config)
 
 	ListPods(clientset, namespace)
-	CreatePod(clientset, podName, imageName, namespace) 
+	CreatePod(clientset, podname, imagename, namespace) 
 	ListPods(clientset, namespace)
-	UpdatePod(clientset, podName, newImageName, namespace)
-	// ListPods(clientset, namespace)
-	// DeletePod(clientset, podName, namespace) 
+	UpdatePod(clientset, podname, newImageName, namespace)
+	ListPods(clientset, namespace)
+	DeletePod(clientset, podname, namespace) 
 	//here after deleting pod the list pod will show the the pod didn't delete
 	//it because the state of deleted pod didn't change in etcd
 	//after we checked using kubectl we found that the pod deleted
-	// ListPods(clientset, namespace)
+	//as I checked in pod.status.condition, and after several tests:
+	//the conditions is an array, each part of this array describe an state with a message
+	//the message show either the pod is ready or pod is completed
+	//if the pod is completed, it is in his destroying phase which it is translate to be deleted
+	//it is normal that listpod shows the name of the pod, that is because it isn't deleted but it is destroying
+	//it will take a litte bit of time to will not shows again
+	//etcd keeps the name of the pod because it not deleted 100%
+	//but if we shows the status of the pod, it will be clear the is in phase of destroying
+	ListPods(clientset, namespace)
 }
